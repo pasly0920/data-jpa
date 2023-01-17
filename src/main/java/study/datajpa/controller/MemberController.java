a6956bb9ec7bced67446c1850e31794ae7d91e64
pasly0920 <inwoo920621@gmail.com>
2023-01-17 12:49:17 +0900

    feat: Member Controller 추가

---
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,43 @@
+package study.datajpa.controller;
+
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        return memberRepository.findById(id).get().getUsername();
+    }
+
+    @GetMapping("/members2/{id}") //단순 조회만 가능 트랜젝션이 없는 곳에서 불러왔기에
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable)
+                .map(MemberDto::new);
+    }
+
+    @PostConstruct
+    public void init() {
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i, null));
+        }
+    }
+}
