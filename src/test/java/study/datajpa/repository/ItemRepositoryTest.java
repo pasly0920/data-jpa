ba73ced3c482a02b45f3b50143de93af331bfa4e
pasly0920 <inwoo920621@gmail.com>
2023-01-17 15:57:27 +0900

    feat: item repository

---
+++ src/test/java/study/datajpa/repository/ItemRepositoryTest.java
@@ -0,0 +1,11 @@
+package study.datajpa.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+}
